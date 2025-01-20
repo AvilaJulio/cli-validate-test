eefc25a253670cc729c347339513f1dd87dadeeb
AvilaJulio <julioley32@gmail.com>
2025-01-20 06:45:52 -0600

    init

---
+++ model/orders.js
@@ -0,0 +1,68 @@
+cube(`Orders`, {
+    sql: `SELECT *
+          FROM public.orders`,
+
+    preAggregations: {
+        // Pre-Aggregations definitions go here
+        // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started
+        main: {
+            measures: [
+                CUBE.count,
+                CUBE.cd,
+            ],
+            timeDimension: CUBE.createdAt,
+            granularity: `day`
+        }
+    },
+
+    measures: {
+        count: {
+            type: `count`,
+            drillMembers: [id, createdAt]
+        },
+
+        number: {
+            sql: `number`,
+            type: `sum`
+        },
+
+        cd: {
+            sql: `user_id`,
+            type: `countDistinctApprox`
+        }
+    },
+
+    dimensions: {
+        id: {
+            sql: `id`,
+            type: `number`,
+            primaryKey: true
+        },
+
+        status: {
+            sql: `status`,
+            type: `string`
+        },
+
+        status_test: {
+          sql: 'status',
+          type: `string`
+        },
+
+        createdAt: {
+            sql: `created_at`,
+            type: `time`
+        },
+
+        completedAt: {
+            sql: `completed_at`,
+            type: `time`
+        },
+
+        user_id: {
+            sql: `user_id`,
+            type: `number`
+        }
+    },
+
+});
